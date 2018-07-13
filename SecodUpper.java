49056271bfade4e08f5a2babae7c3ce6968f18bb
Shubham Kumar <34469341+PIR4T3@users.noreply.github.com>
2018-07-13 11:00:45 +0530

    Update SecodUpper.java

--- SecodUpper.java
+++ SecodUpper.java
@@ -6,11 +6,11 @@
  */
 
 class SecondUpper{
-   public static void main(String[] args) {
+   public void upper(String str) {
       int i=0;
-      int n = args[0].length();
+      int n = str.length();
       for(i=0;i<n;i++){
-         if(args[0].charAt(i) == ' ')
+         if(str.charAt(i) == ' ')
             break;
          }
       if(n==i){
@@ -19,10 +19,18 @@ class SecondUpper{
 
       else{
             i = i+1;
-            while(i<n && args[0].charAt(i)!=' '){
-               System.out.print(Character.toUpperCase(args[0].charAt(i)));
+            while(i<n && str.charAt(i)!=' '){
+               System.out.print(Character.toUpperCase(str.charAt(i)));
                i++;
             }
       }
    }
+}
+
+class Main{
+      public static void main(String[] args) {
+            String str = args[0];
+            SecondUpper u = new SecondUpper();
+            u.upper(str);
+      }
 }

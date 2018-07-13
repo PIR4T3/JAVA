46635d852a7c2f594929cc0505c5d1c2ae6074f8
Shubham Kumar <34469341+PIR4T3@users.noreply.github.com>
2018-07-13 10:55:21 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -2,16 +2,14 @@
  * Print Pattern as follows:
          
          1*2*3*4*17*18*19*20
-	   5*6*7*14*15*16
-	       8*9*12*13
-	  	10*11
-		
+			  5*6*7*14*15*16
+			    8*9*12*13
+			     10*11
  */
 
 class Pattern{
-   public static void main(String args[]){
-      int n, k, temp, i, j, x = 1;
-      n = Integer.parseInt(args[0]);
+   public void pattern(int n){
+      int  k, temp, i, j, x = 1;
       k = n*n + 1;
       temp = k;
       for(i=0;i<n;i++){
@@ -32,4 +30,13 @@ class Pattern{
          System.out.println();
       }
    }
+}
+
+class Main{
+  public static void main(String[] args) {
+    int n;
+    n = Integer.parseInt(args[0]);
+    Pattern p = new Pattern();
+    p.pattern(n);
+  }
 }
